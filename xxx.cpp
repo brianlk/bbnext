cb235d98c7406f11079eb4df0c24263736c8681d
Brian Leung <brian.leungkp@gmail.com>
2023-05-06 17:46:25 +0800

    add class

--- xxx.cpp
+++ xxx.cpp
@@ -1,20 +1,30 @@
 #include <cstdio>
+#include <vector>
 
 int func1 (int x);
 
 using namespace std;
 
+class Memory {
+	public:
+		int memory_size;
+
+		void check_memory() {
+			printf("check memory.");
+		}
+};
+
 int main() {
-	int x = 1;
-	// printf("i am %d", x);
-	func1(1);
-	printf("%p\n", &x);
-	int *ptr = &x;
-	printf("%p", ptr);
-	printf("%d", *ptr);
+	vector<int> v1 = {1, 2};
+	for (const int &i : v1) {
+		printf("%d\n", i);
+	}
+	Memory mem_oj;
+	mem_oj.check_memory();
 	return 0;
 }
 
 int func1 (int x) {
+
 	return x;
 }
