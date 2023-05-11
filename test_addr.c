e4faae7b4abc1c3f9aba1ba124f93298a0e9d746
Brian Leung <brian.leungkp@gmail.com>
2023-05-11 22:56:41 +0800

    update

--- test_addr.c
+++ test_addr.c
@@ -16,4 +16,25 @@ int main() {
 	// dynamic mem allocation
 	int* ptr = (int*)malloc(1000000 * sizeof(int));
 	ptr[0] = 10000;
+
+	// print array name
+	int arr[3] = {1,2,3};
+	printf("arr name pointer constant: %p\n", arr);
+	printf("first element address: %p\n", &arr[0]);
+
+  // c_arr is pointer constant and cannot be changed
+	char c_arr[100] = "i am okay";
+	printf("%s\n", c_arr);
+	// dereference a pointer c_arr = first char
+	printf("%c\n", *c_arr);
+	printf("%p\n", c_arr);
+	// use char pointer and string assginment without strcopy
+  // c_p is not a constant so it can be changed.
+	char* c_p = "i am okay";
+	printf("%s\n", c_p);
+	printf("%c\n", c_p[0]);
+	printf("%p\n", c_p);
+	c_p = "i am not okay";
+	printf("%s\n", c_p);
+	printf("%p\n", c_p);
 }
