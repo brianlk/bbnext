0555e89c6050553d698c60c4230a9a3016ee2540
Brian Leung <brian.leungkp@gmail.com>
2023-05-11 15:55:01 +0800

    update

--- getchar_2.c
+++ getchar_2.c
@@ -2,10 +2,16 @@
 #include "string.h"
 
 int main() {
+    char output[5];
+    char out2[10] = "xxx";
     int first_init, last_init;
     printf("What are your two initials?\n");
     first_init = getchar();
     last_init = getchar();
-    printf("%c", (char)first_init);
-    printf("%c", (char)last_init);
+    output[0] = (char)first_init;
+    output[1] = (char)last_init;
+    printf("%s\n", output);
+    strcat(out2, output);
+    printf("%s\n", out2);
+    return 0;
 }
