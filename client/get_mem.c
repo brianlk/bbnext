5169bee5e2575a5d06a285fa63307522c500a88d
Brian Leung <brian.leungkp@gmail.com>
2023-05-29 11:50:54 +0800

    move code to function

--- client/get_mem.c
+++ client/get_mem.c
@@ -1,27 +1,24 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "stdbool.h" 
+
 
 #define MEMINFO "/proc/meminfo"
 #define BUFFERSIZE 64
 
-int main() {
-	FILE *fp;
-	char buffer[BUFFERSIZE];
 
-	// open "/proc/meminfo"
-	fp = fopen(MEMINFO, "r");
-	if (fp == NULL) {
-		puts("Error: Can't open the file!");
-		exit(1);
-	}
+
+bool extract_lines(FILE *fp) {
+	char buffer[BUFFERSIZE];
+	
 	// Read the file until eof
 	while (!feof(fp)) {
 		fgets(buffer, BUFFERSIZE, fp);
 		if (buffer == NULL)
 			break;
 		// grep the related string
-		char *target = strndup(buffer, 5);	
+		char *target = strndup(buffer, 5);
 		if (strcmp(target, "MemTo") == 0){
 			printf("%s\n", target);
 		}
@@ -29,8 +26,22 @@ int main() {
 			printf("%s\n", target);
 		}
 	}
+	return true;
+}
+
+
+
+int main() {
+	FILE *fp;
+
+	// open "/proc/meminfo"
+	fp = fopen(MEMINFO, "r");
+	if (fp == NULL) {
+		puts("Error: Can't open the file!");
+		exit(1);
+	}
+	extract_lines(fp);
 	fclose(fp);
 
-	// abcd
 	return(0);
 }
