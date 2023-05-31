782cc272368915c62b2511553e6bc3322d30fb4a
Brian Leung <brian.leungkp@gmail.com>
2023-05-31 18:12:40 +0800

    use array of pointer

--- client/get_mem.c
+++ client/get_mem.c
@@ -112,10 +112,12 @@ int main() {
   fclose(fp);
 
   for (int i=0; i<counter; i++) {
+    // print the objects in queue
     printf("%s => %s\n", (sj_queue[i])->key, (sj_queue[i])->value);
+    // release the previous allocated address
+    free(sj_queue[i]);
   }
-  // print the objects in queue
 
-  // free(sj_queue);
+
   return EXIT_SUCCESS;
 }
