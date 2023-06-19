5173000e04a6ce2410a96642e0c39f1427033661
Brian Leung <brian.leungkp@gmail.com>
2023-06-19 18:42:59 +0800

    add global store

--- backend/engines/hosts.py
+++ backend/engines/hosts.py
@@ -1,9 +1,15 @@
 import json
+from db.store import store
 
 def hosts():
     hosts = [
         {"hostname":"host1", "cpu":1, "memory":1},
         {"hostname":"host2", "cpu":0, "memory":0},
     ]
+    global store
     return hosts
     
+def add_host():
+    x = {"hostname":"host1", "cpu":1, "memory":1}
+    global store
+    store.append(x)
--- backend/web_ui/main.py
+++ backend/web_ui/main.py
@@ -1,4 +1,4 @@
-from engines.read import hosts
+from engines.hosts import hosts, add_host
 
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
@@ -8,8 +8,16 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html', hosts=hosts(), request=request)
+    xxx = hosts()
+    if len(xxx) > 0:
+        return render_template('index.html', hosts=xxx, request=request)
+    return "<h1>No hosts</h1>"
 
+@app.route('/add')
+def add():
+    add_host()
+    return '<h1>aaa</h1>'
+    
 
 @app.route('/host/<host_id>')
 def host(host_id):
