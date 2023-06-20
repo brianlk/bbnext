e6c36184651c89b5cc2ebaabb2066183d54fb3dd
Brian Leung <brian.leungkp@gmail.com>
2023-06-20 18:27:03 +0800

    add db logic

--- backend/start_main.sh
+++ backend/start_main.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+
+if [ $# -eq 0 ]; then
+    echo "Usage: ./start_main.sh DEV"
+    exit 1
+fi
+
 export PYTHONPATH=$PWD
 export FLASK_APP=web_ui/main.py
 export FLASK_DEBUG=0
+export FLASK_RUNTIME_ENV=$1
+
 flask run --host 0.0.0.0
--- backend/web_ui/app_config.py
+++ backend/web_ui/app_config.py
@@ -1,6 +1,19 @@
+import os
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abcd1234'
 bootstrap = Bootstrap(app)
+
+runtime_env = os.getenv('FLASK_RUNTIME_ENV')
+
+import sqlalchemy as db
+if runtime_env == "DEV":
+    DBNAME = 'db/abcd.sqlite'
+elif runtime_env == "PROD":
+    pass
+
+engine = db.create_engine(f'sqlite:///{DBNAME}')
+connection = engine.connect()
